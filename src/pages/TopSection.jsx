5501c176061068f73f2f8d29d363038be71c116d
Bhargav <bhargavpasupuleti5@gmail.com>
2026-01-29 22:16:26 +0530

    font correction

--- src/pages/TopSection.jsx
+++ src/pages/TopSection.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const TopSection = () => {
   return (
-    <div className=" fadeAnimation  h-fit p-2  w-full flex justify-center items-center flex-col">
+    <div className=" fadeAnimation font-[poppins] bg-[rgb(14,9,17)]  h-fit p-2  w-full flex justify-center items-center flex-col">
       <div className="flex items-center space-x-4 w-full justify-center">
-        <p className="font-bold  fadeAnimation text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-[rgb(255,255,255)]">
+        <p className="font-bold font-[poppins]  fadeAnimation text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-[rgb(255,255,255)]">
           hey!
         </p>
       </div>
       <br />
       <p className=" fadeAnimation text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
         i'm{" "}
-        <span className=" font-poppins font-bold   text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl text-[rgb(171,71,202)]">
+        <span className=" font-[poppins] font-bold   text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl text-[rgb(171,71,202)]">
           Bhargav
         </span>
       </p>
